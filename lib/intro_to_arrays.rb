a7e1fe71da0a85d308edd99cca9e6acc3e633064
Jessica Gladstone <jcg321@comcast.net>
2018-09-27 14:52:54 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,5 +19,14 @@ end
 def last_element (arr)
   return arr[-1]
 end
+
+def first_element_with_array_methods (arr)
+ return arr.first 
+end
+
+def last_element_with_array_methods
+  return arr.last 
+end
+  
   
   
